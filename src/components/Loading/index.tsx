1f96958af9d1e5496eec8204f251f707e0b4ab14
Anne Lee Steele <aleesteele@gmail.com>
2021-08-17 11:51:23 +0200

    changed library

--- src/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss";
-
-const Loading = (): JSX.Element => (
-  <div className={styles.Loading}>
-    <p>Loading...</p>
-  </div>
-);
-
-export default Loading;
--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import List from "./List";
-import Info from "./Info";
-import ErrorBoundary from "./ErrorBoundary";
-import Loading from "./Loading";
-
-export { List, Info, ErrorBoundary, Loading };
